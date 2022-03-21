e10e389632e00af7465c50f6ace3ed5897ed0752
EnrZ <ez_25_25@yahoo.com>
2022-03-20 21:42:37 -0500

    Made most of the DOM and events studio w/ Angular

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,55 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  newBottomLocation = 0;
+
+  takeOff(){
+    let result = confirm("Confirm that the shuttle is ready for takeoff.");
+   
+    if(result === true){
+		  this.message = "Shuttle in flight.";
+		  this.color = "blue";
+		  this.height += 10000;
+		}
+  }
+
+  landShip(){
+    alert("The shuttle is landing. Landing gear engaged.");
+    this.message = "The shuttle has landed.";
+    this.color = "green";
+    this.height = 0;
+  }
+
+  abortMission(){
+    let result = confirm("Confirm that you want to abort the mission.");
+  
+    if(result === true){
+	    this.message = "Mission aborted.";
+	    this.color = "green";
+	    this.height = 0;
+    }
+  }
+
+  moveUp(rocketImg){
+    const moveByThisAmountOfPixels = 10;
+    this.newBottomLocation = this.newBottomLocation += moveByThisAmountOfPixels;
+    rocketImg.style.bottom = this.newBottomLocation + "px";
+    this.height += 10000;
+  }
+
+
+  moveDown(rocketImg){
+    const moveByThisAmountOfPixels = 10;
+    this.newBottomLocation = this.newBottomLocation -= moveByThisAmountOfPixels;
+    rocketImg.style.bottom = this.newBottomLocation + "px";
+    this.height -= 10000;
+  }
+
+
+  resetRocketPos(rocketImg){
+    rocketImg.style.bottom = "0px";
+    rocketImg.style.left = "0px";
+  }
 }
+
+
